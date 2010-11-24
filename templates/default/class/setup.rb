33ca45d14db8ada3799e618a4c55443f60ea22e8
Sebastian Staudt <koraktor@gmail.com>
2010-11-24 07:51:41 +0100

    Added command details

--- templates/default/class/setup.rb
+++ templates/default/class/setup.rb
@@ -7,6 +7,7 @@ def init
   super
   sections.place(:rubikon).after(T('docstring'))
   sections.place(:command_summary).after(:attribute_summary)
+  sections.place(:command_details, [T('command_details')]).after(:method_details_list)
 end
 
 def commands
---
+++ templates/default/command_details/setup.rb
@@ -0,0 +1,8 @@
+# This code is free software; you can redistribute it and/or modify it under
+# the terms of the new BSD License.
+#
+# Copyright (c) 2010, Sebastian Staudt
+
+def init
+  sections :header, [:command_signature, T('docstring')]
+end
